d7b9ca6a3fb67e4ba4655ac74592fb341fef05fd
Simon GALASSO <sgalasso@e1r12p13.42.fr>
2020-02-27 15:58:28 +0100

    [SG] Parsing refactor

--- src/main.rs
+++ src/main.rs
@@ -24,41 +24,18 @@ struct Move {
 
 fn main() {
     let _args: Vec<String> = env::args().collect();
-    let mut _moves: Vec<Move> = Vec::new();
 
     if _args.len() != 2 {
         println!("[!] Error, Rubik should receive one argument");
         return ;
     }
 
-    let _arg_list: Vec<&str> = _args[1].trim().split_whitespace().collect();
+    let _moves: Vec<Move> = match get_moves_list(&_args) {
+        Some(value) => value,
+        None => return
+    };
 
-    for index in 0.._arg_list.len() {
-        _moves.push(Move {
-            face: match _arg_list[index].chars().next().unwrap() {
-                'F' => Face::Front,
-                'R' => Face::Right,
-                'U' => Face::Up,
-                'B' => Face::Bot,
-                'L' => Face::Left,
-                'D' => Face::Down,
-                _ => {
-                    println!("[!] Error, Patern does not match: {}", _arg_list[index]);
-                    return ;
-                },
-            },
-            action: match &_arg_list[index][1..] {
-                "" => Action::RotRight,
-                "'" => Action::RotLeft,
-                "2" => Action::DoubleRot,
-                _ => {
-                    println!("[!] Error, Patern does not match: {}", _arg_list[index]);
-                    return ;
-                },
-            }
-        })
-    }
-
+    // [!] Debug : Display given Moves
     for _move in _moves {
         match _move.face {
             Face::Front => print!("Front"),
@@ -75,4 +52,36 @@ fn main() {
             Action::DoubleRot => println!("DoubleRot"),
         }
     }
+}
+
+fn get_moves_list(_args: &Vec<String>) -> Option<Vec<Move>> {
+    let _arg_list: Vec<&str> = _args[1].trim().split_whitespace().collect();
+    let mut _moves: Vec<Move> = Vec::new();
+
+    for index in 0.._arg_list.len() {
+        _moves.push(Move {
+            face: match _arg_list[index].chars().next().unwrap() {
+                'F' => Face::Front,
+                'R' => Face::Right,
+                'U' => Face::Up,
+                'B' => Face::Bot,
+                'L' => Face::Left,
+                'D' => Face::Down,
+                _ => {
+                    println!("[!] Error, Patern does not match: {}", _arg_list[index]);
+                    return None;
+                },
+            },
+            action: match &_arg_list[index][1..] {
+                "" => Action::RotRight,
+                "'" => Action::RotLeft,
+                "2" => Action::DoubleRot,
+                _ => {
+                    println!("[!] Error, Patern does not match: {}", _arg_list[index]);
+                    return None;
+                },
+            }
+        })
+    }
+    Some(_moves)
 }
