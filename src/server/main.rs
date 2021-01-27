919a4ba920638f81b2f4b97ed15c425cc82045d5
nicolasvienot <nvienot@student.42.fr>
2021-01-27 19:18:35 +0100

    server working, json implemented

--- src/server/main.rs
+++ src/server/main.rs
@@ -1,4 +1,16 @@
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{get, post, web::{self, Json, Path}, App, HttpResponse, HttpServer, Responder, Result};
+use serde::{Serialize, Deserialize};
+
+#[derive(Deserialize)]
+struct Info {
+    username: String,
+}
+
+/// extract `Info` using serde
+#[post("/json")]
+async fn json(info: Json<Info>) -> impl Responder {
+    HttpResponse::Ok().body(format!("Hello, {}", info.username))
+}
 
 #[get("/")]
 async fn hello() -> impl Responder {
@@ -15,15 +27,42 @@ async fn manual_hello() -> impl Responder {
     HttpResponse::Ok().body("Hey there!")
 }
 
+#[post("/shuffle")]
+async fn shuffle(req_body: String) -> impl Responder {
+    let shuffle: String = "D D D D D D'".to_string();
+    HttpResponse::Ok().body(shuffle)
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    print_launch_resume();
     HttpServer::new(|| {
         App::new()
             .service(hello)
             .service(echo)
+            .service(shuffle)
+            .service(json)
             .route("/hey", web::get().to(manual_hello))
     })
     .bind("127.0.0.1:8080")?
     .run()
     .await
+}
+
+pub fn print_launch_resume() {
+    let ascii_name = r#" *******           **      ** **     **        
+/**////**         /**     // /**    //*        
+/**   /**  **   **/**      **/**  ** /   ******
+/*******  /**  /**/****** /**/** **     **//// 
+/**///**  /**  /**/**///**/**/****     //***** 
+/**  //** /**  /**/**  /**/**/**/**     /////**
+/**   //**//******/****** /**/**//**    ****** 
+//     //  ////// /////   // //  //    //////  "#;
+    eprintln!("{}", ascii_name);
+    eprintln!();
+    eprintln!("Server listening on:\t{:?}", "127.0.0.1:8080");
+    eprintln!();
+    eprintln!("Source code:\t\thttps://github.com/sgalasso42/rubik");
+    eprintln!("Contact:\t\thttps://github.com/sgalasso42/rubik or bonjour@rubik.com");
+    eprintln!();
 }
