82a9b7443d20671d026bbb710883c507a8e4d8ef
nicolasvienot <nvienot@student.42.fr>
2021-02-03 16:50:06 +0100

    Server working with static files

--- src/server/main.rs
+++ src/server/main.rs
@@ -1,17 +1,14 @@
-use actix_web::{get, post, web::{self, Json, Path}, App, HttpResponse, HttpServer, Responder, Result};
+extern crate env_logger;
+use actix_cors::Cors;
+use actix_web::{middleware, http, get, post, web::{self, Json, Path}, App, HttpRequest, HttpResponse, HttpServer, Responder, Result};
 use serde::{Serialize, Deserialize};
 use rand::Rng;
+use actix_files::Files;
+use std::path::PathBuf;
 
 #[derive(Deserialize)]
 struct Info {
     username: String,
-}
-
-#[get("/")]
-pub async fn load_html() -> impl Responder {
-    HttpResponse::Ok()
-        .content_type("text/html; charset=utf-8")
-        .body(include_str!("./public/welcome.html").to_string())
 }
 
 #[post("/json")]
@@ -28,12 +25,23 @@ async fn scramble() -> impl Responder {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    std::env::set_var("RUST_LOG", "actix_web=info");
+    env_logger::init();
     print_launch_resume();
     HttpServer::new(|| {
         App::new()
-            .service(load_html)
+            .wrap(middleware::Logger::default())
+            .wrap(
+                Cors::new()
+                .allowed_origin("http://localhost:3000")
+                .allowed_methods(vec!["GET", "POST"])
+                .allowed_headers(vec![http::header::AUTHORIZATION, http::header::ACCEPT])
+                .allowed_header(http::header::CONTENT_TYPE)
+                .max_age(3600)
+                .finish())
             .service(scramble)
             .service(json)
+            .service(Files::new("/", "./src/server/static/root/").index_file("index.html"))
     })
     .bind("127.0.0.1:8080")?
     .run()
